bca4a7ba3456c756c856f373f74f487de14a9fb1
Raghu P Pushpakath <ppraghu@yahoo.com>
2022-09-11 20:34:20 +1000

    Added CSRF Protection

--- sample_flask_site/sample_site.py
+++ sample_flask_site/sample_site.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 @app.route('/')
+@csrf.exempt # Sensitive
 def hello():
     html_page = '<html><title>Sample flask website</title><body><h2>This is a sample website created with Flask and Python</h2></body></html>';
     return html_page;
