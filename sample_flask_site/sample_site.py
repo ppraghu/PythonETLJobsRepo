a1a712f335f5079ce1e79735b773e70e7599e4b9
Raghu P Pushpakath <ppraghu@yahoo.com>
2022-09-11 20:40:16 +1000

    Changed the html output

--- sample_flask_site/sample_site.py
+++ sample_flask_site/sample_site.py
@@ -8,6 +8,6 @@ csrf.init_app(app)
 @app.route('/')
 @csrf.exempt # Sensitive
 def hello():
-    html_page = '<html><title>Sample flask website</title><body><h2>This is a sample website created with Flask and Python</h2></body></html>';
+    html_page = '<html><title>Sample flask website</title><body><h2>This is a sample website created with Flask and Python</h2><p><h3>Created by UST Team</h3></body></html>';
     return html_page;
 
